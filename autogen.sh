236c0dd158c8307a9eb96b916128c01c166bd037
Denys Misko <denysmisko@DraalMac.local>
2014-03-21 17:49:02 +0200

    Change the lib url from ssh to https

--- autogen.sh
+++ autogen.sh
@@ -3,15 +3,6 @@
 if [ -d .git ]; then
 	git pull 
 	git submodule init	
-	https=`git config --get remote.origin.url | grep https`
-	if [ "${https}" != "" ]
-	then
-			cur=`git config submodule.fl_libs.url | grep git@ | tr ":" "/"`
-			if [ "${cur}" != "" ]
-			then
-					git config submodule.fl_libs.url https://${cur:4}
-			fi
-	fi
 	git submodule update && git submodule status
 	currentBranch=`git rev-parse --abbrev-ref HEAD`
 	cd fl_libs && git checkout ${currentBranch}
